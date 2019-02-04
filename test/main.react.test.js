5b91243cc9c0fc22a06b151d22f5c939a42c4e61
unknown <mregydev@gmail.com>
2019-02-04 20:57:19 +0300

    added test

--- test/main.react.test.js
+++ test/main.react.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import main from './src/Components/Container/main';
 import renderer from 'react-test-renderer';
+import mainComp from '../src/Components/Container/main.jsx';
 
 test('main window should be rendered', () => {
   const component = renderer.create(
-    <main></main>,
+    <mainComp></mainComp>,
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+
+
